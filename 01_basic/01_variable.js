4143467ecae7a66a19d4a1857ec52c5f4276b752
Vikash Kumar <vikash9534422068@gmail.com>
2025-12-26 12:43:25 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = "0124568"
+let accountEmail = "vikash@4567.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+// accountId = 2 // not change another accountId why the first accountId id const so not change your account
+console.log(accountId);
