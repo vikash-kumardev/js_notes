31a225a72ef2e15d3ca3dcbd9bfbe06c17d41269
Vikash Kumar <vikash9534422068@gmail.com>
2025-12-28 17:35:28 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version 
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3+3)//likhne ka sahi tarika
+console.log(3
+    +
+    3)// ye likhne ka tarika sahi nhi h
+    console.log("vikash")
+
+
+
+    let name ="vikash"
+    let age =22
+    let isLoggedIn = false
+
+    //Primitive Data types
+   // number => 2 to power 53 (itna number ka size tk likh skte h )
+   //bigint => large size of number (ex => facebook,youtube)
+   //string => " "
+   //boolean => true/false
+   //null => standalone value
+   //undefined => 
+   //symbol => unique
+   //object
+
+   console.log(typeof age); //koi bhi key point le skte h
+   // (ex => age, name, isLoggedIn, null, undefined)
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
+
+
+   //NonPrimitive datatype
