38f4c049951d8b5ba4e82b71361befbd0f91e878
Vikash Kumar <vikash9534422068@gmail.com>
2025-12-29 15:00:18 +0000

    conversiondattype

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,3 +23,28 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = " Vikash"
+let str3 = str1 + str2
+console.log(str3)// output= hello Vikash
+
+console.log("1" + 2);//output = 12
+console.log(1 + "2");//output = 12
+console.log("1" + 2 + 2);//output = 122
+console.log(1 + 2 + "2");//output = 32
+
+console.log(true);//output = true
+console.log(+true); //output = 1
+console.log(+""); //output = 0
+//but this not write formate 
+//this is not the correct way to write it
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.
